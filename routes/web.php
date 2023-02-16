7912c929ccb0f58078b0f478d06b8521a3715cd3
Reza Ghiasi <imrezaghiasi@gmail.com>
2023-02-16 15:53:11 +0330

    bug fixed

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('blog.index');
+Route::get('/',function (){
+    return redirect(\route('blog.index'));
 });
 
 Route::get('/dashboard', function () {
